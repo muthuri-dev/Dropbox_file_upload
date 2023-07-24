f94980f3ecf050c36208cd1dbfa858ab8348af4b
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-07-24 23:11:38 +0300

    added dropbox api

---
+++ Pages/UploadBase.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+using Dropbox.Api;
+
+namespace Dropbox_file_upload.Pages;
+
+public class UploadBase:ComponentBase
+{
+    static string token = "sl.Bi3jfA-JsEwnTWy9inm9i7Vcvb2YjfvVlALtq993kBJZg6gcNyag6aUEz4Edr-Asmn79F11QBHIGpQZlucxRUO4FSaQSWU7aYcVb7oQWbfbU_RpzyOsTJsgTgc5DdtteKTqszkmPYRld";
+}
